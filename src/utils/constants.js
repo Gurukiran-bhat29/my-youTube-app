cb1d2911941725d583b622a6148ed472d66ece37
Gurukiran <gurukiran.bhat@niveussolutions.com>
2023-03-27 20:19:12 +0530

    .env creation

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,5 @@
-// My Youtube api key = AIzaSyBF2ibTTc6nVOe_Is63RCXRcpR523-gRms;
-const MY_GOOGLE_API_KEY = 'AIzaSyBF2ibTTc6nVOe_Is63RCXRcpR523-gRms';
-
 export const YOUTUBE_VIDEO_API =
-  'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&&maxResults=50&regionCode=IN&key=' + MY_GOOGLE_API_KEY;
+  'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&&maxResults=50&regionCode=IN&key=' + process.env.REACT_APP_GOOGLE_API_KEY;
 ;
 
 export const Lists = ['All', 'Gaming', 'Songs', 'Live', 'Cricket', 'Football', 'Mixes', 'News', 'Comedy'];
