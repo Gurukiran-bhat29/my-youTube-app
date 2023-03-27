86dbcdf1272b49f1d69c41640fd60ee41eee4f4f
Gurukiran <gurukiran.bhat@niveussolutions.com>
2023-03-27 21:34:01 +0530

    home icon added

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -9,31 +9,34 @@ const SideBar = () => {
   if (!isMenuOpen) return null;
 
   return (
-    <div className="shadow-lg">
-      <div className="p-2.5 border-b-2 border-gray-400">
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li>Shorts</li>
-          <li>Videos</li>
-          <li>Live</li>
+    <div className="mx-2">
+      <div className="py-2.5 px-4 border-b-[1px] border-gray-300">
+        <ul className="text-center">
+          <Link to='/' className="flex">
+            <img className="w-8 h-8" alt='Home' src='https://cdn-icons-png.flaticon.com/512/25/25694.png' />
+            <li className="py-2 px-4 text-gray-800">Home</li>
+          </Link>
+          <li className="py-2 text-gray-800">Shorts</li>
+          <li className="py-2 text-gray-800">Videos</li>
+          <li className="py-2 text-gray-800">Live</li>
         </ul>
       </div>
-      <div className="p-2.5 border-b-2 border-gray-400">
-        <p className="font-bold">Subscriptions</p>
-        <ul>
-          <li>Music</li>
-          <li>Sports</li>
-          <li>Gaming</li>
-          <li>Movies</li>
+      <div className="py-2.5 px-4 border-b-[1px] border-gray-300">
+        <p className="font-bold py-1">Subscriptions</p>
+        <ul className="text-center">
+          <li className="py-2 text-gray-800">Music</li>
+          <li className="py-2 text-gray-800">Sports</li>
+          <li className="py-2 text-gray-800">Gaming</li>
+          <li className="py-2 text-gray-800">Movies</li>
         </ul>
       </div>
-      <div className="p-2.5">
-        <p className="font-bold">Watch Later</p>
-        <ul>
-          <li>Music</li>
-          <li>Sports</li>
-          <li>Gaming</li>
-          <li>Movies</li>
+      <div className="py-2.5 px-4">
+        <p className="font-bold py-1">Watch Later</p>
+        <ul className="text-center">
+          <li className="py-2 text-gray-800">Music</li>
+          <li className="py-2 text-gray-800">Sports</li>
+          <li className="py-2 text-gray-800">Gaming</li>
+          <li className="py-2 text-gray-800">Movies</li>
         </ul>
       </div>
     </div>
