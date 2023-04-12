f4493c4608d20dbaedfd872f82c512b56914db27
Gurukiran <gurukiran.bhat@niveussolutions.com>
2023-04-12 18:22:37 +0530

    fixes

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -7,7 +7,7 @@ const VideoCard = ({ info }) => {
   const isMenuOpen = useSelector(store => store.app.isMenuOpen);
 
   return (
-    <div className={`p-2 m-2 shadow-lg ${!isMenuOpen ? 'w-[335px]' : 'w-96'}`}>
+    <div className={`p-2 m-2 shadow-lg w-96 md:w-80 lg:${!isMenuOpen ? 'w-[335px]' : 'w-96'}`}>
       <img className='rounded-lg w-full' alt='tumb-nail' src={thumbnails.medium.url} />
       <ul>
         <li className="font-bold py-2">{title}</li>
