26d9b3e57346232ebeaf93d1363d89ee1ae4f153
Gurukiran <gurukiran.bhat@niveussolutions.com>
2023-04-12 20:28:45 +0530

    Css changes

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,13 +1,9 @@
-import { useSelector } from "react-redux";
-
 const VideoCard = ({ info }) => {
   const { snippet, statistics } = info;
   const { channelTitle, title, thumbnails } = snippet;
 
-  const isMenuOpen = useSelector(store => store.app.isMenuOpen);
-
   return (
-    <div className={`p-2 m-2 shadow-lg w-96 md:w-80 lg:${!isMenuOpen ? 'w-[335px]' : 'w-96'}`}>
+    <div className={`p-2 m-2 shadow-lg w-96 sm:w-96 md:w-80 lg:w-80 xl:w-96`}>
       <img className='rounded-lg w-full' alt='tumb-nail' src={thumbnails.medium.url} />
       <ul>
         <li className="font-bold py-2">{title}</li>
