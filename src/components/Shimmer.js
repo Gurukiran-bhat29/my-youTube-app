fb3f7a811fbbe2da72869d2b226a37f4c3d4250a
Gurukiran <gurukiran.bhat@niveussolutions.com>
2023-04-27 18:11:21 +0530

    shimmer changes

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,10 +1,10 @@
 const Shimmer = () => {
   return (
-    <div data-testid="shimmer" style={{ display: "flex", flexWrap: "wrap" }}>
+    <div data-testid="shimmer" className='flex flex-wrap m-4'>
       {Array(10)
         .fill("")
         .map((e, index) => (
-          <div key={'shimmer' + index} style={{ width: '200px', height: '200px', backgroundColor: 'gray' }}></div>
+          <div key={'shimmer' + index} className='w-52 h-48 ml-2 mr-2 bg-gray-200 shadow-xl'></div>
         ))}
     </div>
   )
